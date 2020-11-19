1b06db7259e672ea18f557d67ecd91f0f1c55639
Vitaliy Kuprin <vitaly@surance.io>
2020-11-20 00:25:02 +0200

    refactor: next structure

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withSass = require('@zeit/next-sass');
+const withFonts = require('nextjs-fonts');
+
+module.exports = withSass(withFonts({
+    webpack(config, options) {
+        return config;
+    },
+}));
