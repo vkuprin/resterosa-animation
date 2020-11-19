348dfd42ea6f50ee76e1e45c4e752b4bac1ae66f
Vitaliy Kuprin <vitaly@surance.io>
2020-11-20 00:38:15 +0200

    feat: title for page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import styles from '../styles/Home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>ResteRosa</h1>
-    </div>
+      <>
+          <Head>
+              <title>ResteRosa</title>
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          </Head>
+        <div className={styles.container}>
+          <h1>ResteRosa</h1>
+        </div>
+      </>
   )
 }
